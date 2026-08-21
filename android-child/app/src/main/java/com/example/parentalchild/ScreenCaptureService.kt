3a1b0316daa6524d7a24bfa86781eff2daeebbdc
jasurbek602 <115354796+jasurbek602@users.noreply.github.com>
2026-08-21 16:39:59 +0500

    Update ScreenCaptureService.kt

--- android-child/app/src/main/java/com/example/parentalchild/ScreenCaptureService.kt
+++ android-child/app/src/main/java/com/example/parentalchild/ScreenCaptureService.kt
@@ -3,10 +3,23 @@ package com.example.parentalchild
 import android.app.*
 import android.content.*
 import android.media.projection.MediaProjectionManager
+import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 
 class ScreenCaptureService: Service(){
+ override fun onCreate(){
+  super.onCreate()
+  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+   val channel = NotificationChannel(
+    "capture",
+    "Screen Capture",
+    NotificationManager.IMPORTANCE_LOW
+   )
+   val manager = getSystemService(NotificationManager::class.java)
+   manager.createNotificationChannel(channel)
+  }
+ }
  override fun onStartCommand(intent:Intent?,flags:Int,startId:Int):Int{
   val mgr=getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
   startForeground(100, NotificationCompat.Builder(this,"capture").setContentTitle("Family Guard").setContentText("Screen capture faol").setSmallIcon(android.R.drawable.ic_menu_view).build())
