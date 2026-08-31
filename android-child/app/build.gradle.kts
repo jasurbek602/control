afd90289fee61922d470ad7ebfbfdd39b5ea47db
jasurbek602 <115354796+jasurbek602@users.noreply.github.com>
2026-08-31 08:36:40 +0500

    Update build.gradle.kts

--- android-child/app/build.gradle.kts
+++ android-child/app/build.gradle.kts
@@ -28,4 +28,5 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+implementation("com.google.android.gms:play-services-location:21.3.0")
 }
